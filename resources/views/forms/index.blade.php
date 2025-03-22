b254114a8ee4739ffe6e6007b435210d29c9dfdd
Rafael A. Ortega <raortega8906@gmail.com>
2025-03-22 23:38:11 +0100

    add tailwind but not

--- resources/views/forms/index.blade.php
+++ resources/views/forms/index.blade.php
@@ -1,38 +1,38 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Forms</div>
+<div class="max-w-4xl mx-auto p-6">
+    <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+        <div class="bg-blue-500 text-white text-lg font-semibold px-6 py-4">Forms</div>
 
-                <div class="card-body">
-                    @if ($forms->isEmpty())
-                        <div class="alert alert-warning">No forms found.</div>
-                    @else
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Form Name</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Email</th>
-                                    <th>Phone</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($forms as $form)
-                                    <tr>
-                                        <td>{{ $form->form_name }}</td>
-                                        <td>{{ $form->first_name }}</td>
-                                        <td>{{ $form->last_name }}</td>
-                                        <td>{{ $form->email }}</td>
-                                        <td>{{ $form->phone }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    @endif
+        <div class="p-6">
+            @if ($forms->isEmpty())
+                <div class="bg-yellow-100 text-yellow-800 p-4 rounded-lg">
+                    No forms found
                 </div>
-            </div>
+            @else
+                <div class="overflow-x-auto">
+                    <table class="w-full border border-gray-200 rounded-lg shadow-sm">
+                        <thead>
+                            <tr class="bg-gray-200 text-gray-700">
+                                <th class="px-4 py-3 text-left">Form Name</th>
+                                <th class="px-4 py-3 text-left">First Name</th>
+                                <th class="px-4 py-3 text-left">Last Name</th>
+                                <th class="px-4 py-3 text-left">Email</th>
+                                <th class="px-4 py-3 text-left">Phone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($forms as $form)
+                                <tr class="border-t border-gray-300 hover:bg-gray-100">
+                                    <td class="px-4 py-3">{{ $form->form_name }}</td>
+                                    <td class="px-4 py-3">{{ $form->first_name }}</td>
+                                    <td class="px-4 py-3">{{ $form->last_name }}</td>
+                                    <td class="px-4 py-3">{{ $form->email }}</td>
+                                    <td class="px-4 py-3">{{ $form->phone }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endif
         </div>
     </div>
 </div>
